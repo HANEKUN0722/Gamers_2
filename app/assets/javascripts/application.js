6662db2d0d9145fecdc025f747b77d1ba9651c53
vagrant <vagrant@localhost.localdomain>
2020-10-11 13:19:20 +0000

    aboutページ

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,10 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(document).on('turbolinks:load', function(){
+  $('.slick').slick({
+    autoplay:true,  ←自動再生
+    dots:true,      ←ドットの描写
+  });
+});
